93c7ce6e3e2fce31fa38908b2ece61140bd11a9c
Dmitrii Tonkushin <d.tonkushin@smartcat.ai>
2020-07-13 17:44:23 +0500

    Added ability to launch tests in parallel in different browsers

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,9 +3,11 @@ import com.codeborne.selenide.Screenshots;
 import com.codeborne.selenide.testng.ScreenShooter;
 import com.google.common.io.Files;
 import io.qameta.allure.Attachment;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,12 +17,33 @@ import static com.codeborne.selenide.Selenide.*;
 @Listeners({ ScreenShooter.class})
 public abstract class BaseTest {
 
+    @Parameters("browser")
     @BeforeClass
-    public void beforeClass(){
+    public void beforeClass(String browser) throws Exception {
         Configuration.timeout = 10000;
         Configuration.baseUrl = "https://www.sportmaster.ru";
         Configuration.reportsFolder = "allure-results";
         ScreenShooter.captureSuccessfulTests = true;
+
+        Configuration.remote = "http://localhost:4444/wd/hub";
+
+        if ("chrome".equals(browser)) {
+            Configuration.browser = "chrome";
+        } else if ("firefox".equals(browser)) {
+            Configuration.browser = "firefox";
+        } else if ("opera".equals(browser)) {
+            Configuration.browser = "opera";
+        } else if ("android".equals(browser)) {
+            Configuration.browser = "android";
+        } else {
+            throw new Exception("Произошла ошибка: указанный браузер недоступен.");
+        }
+
+        Configuration.browserSize = "1920x1080";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
 
         clearBrowserCookies();
     }
